df8d9b1265d7ea2d4c4a1facf63da768417d1363
20A91A0162 <106660474+20A91A0162@users.noreply.github.com>
2022-07-29 00:27:00 +0530

    def count_voe(word):     voe='aeiou'     cnt=0     for i in word:         if i in voe:             cnt=cnt+1     return cnt s=input() words=s.split() view=[count_voe(i)for i in words] print(*view)

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,11 @@
+def count_voe(word):
+    voe='aeiou'
+    cnt=0
+    for i in word:
+        if i in voe:
+            cnt=cnt+1
+    return cnt
+s=input()
+words=s.split()
+view=[count_voe(i)for i in words]
+print(*view)
